5484100c3f75204f082043d4e64d86f090f70b96
Thomas Cappelli <thomas.cappelli@gmail.com>
2023-06-14 09:09:22 +0200

    Routes created

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+   # Airlines
+   resources :airlines, only: [:index, :show]
+
+   # Flights
+   resources :flights, only: [:index, :show]
+ 
+   # Bookings
+   resources :bookings, only: [:new, :create, :show]
+ 
+   # Users
+   resources :users, only: [:new, :create]
+ 
+   # Custom Routes
+   get '/search', to: 'flights#search', as: 'flight_search'
 end
