dd8c5f90f0ff4e284d976f6b010301f12f34ab7b
Thomas Cappelli <thomas.cappelli@gmail.com>
2023-06-19 10:01:15 +0200

    Changed the view

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,13 +4,13 @@ require 'json'
 class FlightsController < ApplicationController  
   def search_results
     search_params # Call the method to set the instance variables
-
+  
     access_token = get_access_token('E3QJBG9aB36ZF3tKCopetWORIvV7NYAC', 'Cu94OhFnMVAWGuJo')
     if access_token.nil?
       flash[:alert] = 'Failed to retrieve access token.'
       redirect_to root_path and return
     end
-
+  
     logger.debug "Access Token: #{access_token}"
     logger.debug "Departure City: #{@departure_city}"
     logger.debug "Arrival City: #{@arrival_city}"
@@ -19,18 +19,18 @@ class FlightsController < ApplicationController
     logger.debug "Adults: #{@adults}"
   
     @flights = search_flights(@departure_city, @arrival_city, @departure_date, @return_date, @adults, access_token)
-
-    # @flights = @outbound_flights + @return_flights
-
+  
     @flights.each do |flight|
       flight.flight_time = calculate_flight_time(flight.departure_date, flight.arrival_date)
     end
-
-    logger.debug "Flights: #{flights}"
+  
+    logger.debug "Flights: #{@flights}"
     render 'search_results'
   end
+  
 
 ####### PRIVATE METHOD #######
+
   private
 
   def get_access_token(client_id, client_secret)
@@ -57,9 +57,8 @@ class FlightsController < ApplicationController
     end
   end
 
-  def search_flights(origin, destination, departure_date, return_date, adults access_token)
-    uri = URI("https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=#{origin}&destinationLocationCode=#{destination}&departureDate=#{departure_date}&returnDate=#{return_date}&adults=#{adults}")
-   
+  def search_flights(origin, destination, departure_date, return_date, adults, access_token)
+    uri = URI("https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=#{origin}&destinationLocationCode=#{destination}&departureDate=#{departure_date.strftime('%Y-%m-%d')}&returnDate=#{return_date.strftime('%Y-%m-%d')}&adults=#{adults}")
   
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
@@ -83,16 +82,18 @@ class FlightsController < ApplicationController
         flight.departure_date = Time.parse(flight_offer['itineraries'].first['segments'].first['departure']['at'])
         flight.arrival_date = Time.parse(flight_offer['itineraries'].first['segments'].first['arrival']['at'])
         flight.flight_time = flight_offer['itineraries'].first['duration']
-        flight.price = flight_offer['price']
+        flight.price = flight_offer['price']['total'].to_f # Convert the 'total' value to float
         flights << flight
       end
   
       return flights
     else
       flash[:alert] = 'There was an error with your search. Please try again.'
-      return [ ]
+      return []
     end
   end
+  
+  
   
   def calculate_flight_time(departure_date, arrival_date)
     flight_time = (arrival_date - departure_date) / 1.hour
@@ -104,5 +105,7 @@ class FlightsController < ApplicationController
     @arrival_city = params[:arrival_city]
     @departure_date = Date.parse(params[:departure_date]).beginning_of_day
     @return_date = Date.parse(params[:return_date]).beginning_of_day
+    @adults = params[:adults].to_i
   end
+  
 end
