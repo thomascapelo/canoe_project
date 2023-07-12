1a6d7b86268e32218044caacb5da345cfcfc614c
Thomas Cappelli <thomas.cappelli@gmail.com>
2023-07-12 16:12:32 +0200

    Autocomplete is working again BUT Oneway not working - trying to fix it

--- app/javascript/controllers/form_dropdown.js
+++ app/javascript/controllers/form_dropdown.js
@@ -1,10 +1,12 @@
-const tripTypeDropdown = document.getElementById("trip_type");
-const returnDateInput = document.getElementById("return_date");
+document.addEventListener("DOMContentLoaded", () => {
+  const tripTypeDropdown = document.getElementById("trip_type");
+  const returnDateInput = document.getElementById("return_date");
 
-tripTypeDropdown.addEventListener("change", () => {
-  if (tripTypeDropdown.value === "one_way") {
-    returnDateInput.style.display = "none";
-  } else {
-    returnDateInput.style.display = "block";
-  }
+  tripTypeDropdown.addEventListener("change", () => {
+    if (tripTypeDropdown.value === "one_way") {
+      returnDateInput.style.display = "none";
+    } else {
+      returnDateInput.style.display = "block";
+    }
+  });
 });
